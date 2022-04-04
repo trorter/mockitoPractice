68d197019ae55e43f8128d7b644d2dae67996ff8
andrey <auledovskih@gmail.com>
2022-04-04 16:34:42 +0300

    branch second commit

--- src/main/java/ru/ledovskikh/andrey/App.java
+++ src/main/java/ru/ledovskikh/andrey/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Branch first commit!");
+        System.out.println("Branch second commit!");
     }
 }
